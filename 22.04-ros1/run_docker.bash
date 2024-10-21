fc619a290b8303efce6a9efb17ddea916e7dff27
David Feil-Seifer <dave@cse.unr.edu>
2024-10-21 09:08:37 -0700

    working docker container for noetic that uses code in the home directory

--- 22.04-ros1/run_docker.bash
+++ 22.04-ros1/run_docker.bash
@@ -1,17 +1,33 @@
 ## run this script to enabel GUI
 
-xhost +local:docker
+#xhost +local:docker
+
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
 #xhost +SI:localuser:$(id -un)
 #XAUTH=/tmp/.docker.xauth
 
 docker run -ti -it --rm \
 	-e DISPLAY=$DISPLAY \
 	-e QT_X11_NO_MITSHM=1 \
-	-v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-	dfseifer/ros-test
+	-v $XSOCK:$XSOCK:rw \
+	-v $XAUTH:$XAUTH:rw \
+    -v /home/$USER:/home/$USER \
+    --volume="/etc/group:/etc/group:ro" \
+    --volume="/etc/passwd:/etc/passwd:ro" \
+    --volume="/etc/shadow:/etc/shadow:ro" \
+    --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+    --user="$USER" \
+    dfseifer/ros-test
 
 
-
+    #    --volume=$XAUTH:$XAUTH:rw \
+    #    --env="XAUTHORITY=${XAUTH}" \
+    #    --env="DISPLAY" \
+    #    -user="myNewUserName" \
 		#-e XDG_RUNTIME_DIR=/tmp \
 	#-e WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
 	#-v $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY:/tmp/$WAYLAND_DISPLAY \
