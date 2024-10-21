3f779f5e1490eaedb8403042edf3c811aee33273
David Feil-Seifer <dave@cse.unr.edu>
2024-10-20 23:58:50 -0700

    working x connection

--- 22.04-ros1/run_docker.bash
+++ 22.04-ros1/run_docker.bash
@@ -1,20 +1,20 @@
 ## run this script to enabel GUI
 
 xhost +local:docker
-xhost +SI:localuser:$(id -un)
+#xhost +SI:localuser:$(id -un)
 #XAUTH=/tmp/.docker.xauth
 
-docker run -it \
-	--env=DISPLAY=:0 \
-	--env="QT_X11_NO_MITSHM=1" \
-	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-	--user=$(id -u):$(id -g) \
-	ros_test bash
+docker run -ti -it --rm \
+	-e DISPLAY=$DISPLAY \
+	-e QT_X11_NO_MITSHM=1 \
+	-v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+	dfseifer/ros-test
 
 
 
 		#-e XDG_RUNTIME_DIR=/tmp \
 	#-e WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
 	#-v $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY:/tmp/$WAYLAND_DISPLAY \
+	#--user=$(id -u):$(id -g) \
 
 echo "Done."
